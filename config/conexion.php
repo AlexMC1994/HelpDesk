97e3e22aea15d82287fcd18186d1676a4297e4be
AlexMC1994 <alexmorecrisping@gmail.com>
2021-09-24 18:45:22 -0500

    commit

--- config/conexion.php
+++ config/conexion.php
@@ -1,23 +1,28 @@
 <?php
-session_start();
 
-class Conectar{
-    protected $dbh;
-    protected function Conexion(){
-        try {
-            $conectar = $this->dbh = new PDO("mysql:localhost;dbname=helpdesk", "root" ,"");
-            return $conectar;
-        } catch (Exception $e) {
-            print("Error BD!: ".$e->getmessage()."<br/>");
-            die();
+    session_start();
+
+    class Conectar{
+        protected $dbh;
+
+        protected function Conexion(){
+            try{
+                $conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=helpdesk", "root", "");
+
+                return $conectar;
+            }catch (Exception $e){
+                print "Error BD!: ".$e->getMessage()."<br/>";
+                die();
+            }
+        }
+
+        public function set_names(){
+            return $this->dbh->query("SET NAMES 'utf8'");
+        }
+
+        public static function ruta(){
+            return "http://localhost:80/HelpDesk/";
         }
     }
-    public function set_names(){
-        return $this-> dbh->query("SET NAMES 'tutf8'");
-    }
 
-    public function ruta(){
-        return "http://localhost:8080/helpdesk/";
-    }
-}
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+
+    require_once ("config/conexion.php");
+    if (isset($_POST["enviar"]) and $_POST["enviar"]=="si") {
+        require_once("models/Usuario.php");
+        $usuario = new  Usuario ();
+        $usuario->login ();
+    }
+
+?>
 <!DOCTYPE html>
 <html>
 <head lang="es">
@@ -12,54 +22,73 @@
 	<link href="public/img/favicon.57x57.png" rel="apple-touch-icon" type="image/png">
 	<link href="public/img/favicon.png" rel="icon" type="image/png">
 	<link href="public/img/favicon.ico" rel="shortcut icon">
-
     <link rel="stylesheet" href="public/css/separate/pages/login.min.css">
     <link rel="stylesheet" href="public/css/lib/font-awesome/font-awesome.min.css">
     <link rel="stylesheet" href="public/css/lib/bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="public/css/main.css">
 </head>
 <body>
-
     <div class="page-center">
         <div class="page-center-in">
             <div class="container-fluid">
-                <form class="sign-box">
+                <form class="sign-box" action="" method="post" id="login_form">
                     <div class="sign-avatar">
                         <img src="public/img/avatar-sign.png" alt="">
                     </div>
-                    <header class="sign-title">Sign In</header>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="E-Mail or Phone"/>
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" placeholder="Password"/>
-                    </div>
-                    <div class="form-group">
-                        <div class="checkbox float-left">
-                            <input type="checkbox" id="signed-in"/>
-                            <label for="signed-in">Keep me signed in</label>
+                    <header class="sign-title" id="lbltitulo">Acceso Usuario</header>
+                    <?php
+                    if(isset($_GET["m"])){
+                        switch ($_GET["m"]) {
+                            case '1':
+                            ?>
+                           <div class="alert alert-warning alert-icon alert-close alert-dismissible fade in" role="alert">
+							<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+								<span aria-hidden="true">×</span>
+							</button>
+							<i class="font-icon font-icon-warning"></i>
+							El usuario y/o Contraseña son incorrectos.
+						</div>
+                            <?php
+                            break;
+                            case '2';
+                            ?>
+                            <div class="alert alert-warning alert-icon alert-close alert-dismissible fade in" role="alert">
+							<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+								<span aria-hidden="true">×</span>
+							</button>
+							<i class="font-icon font-icon-warning"></i>
+							Los campos estan vacíos.
                         </div>
+                            <?php
+                            break;
+
+                        }
+                    }
+                    ?>
+                    <div class="form-group">
+                        <input type="text" id="usu_correo" name="usu_correo" class="form-control" placeholder="E-Mail"/>
+                    </div>
+                    <div class="form-group">
+                        <input type="password" id="usu_pass" name="usu_pass" class="form-control" placeholder="Password"/>
+                    </div>
+                    <div class="form-group">
                         <div class="float-right reset">
-                            <a href="reset-password.html">Reset Password</a>
+                            <a href="reset-password.html">Cambiar Contraseña</a>
                         </div>
                     </div>
-                    <button type="submit" class="btn btn-rounded">Sign in</button>
-                    <p class="sign-note">New to our website? <a href="sign-up.html">Sign up</a></p>
-                    <!--<button type="button" class="close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>-->
+                    <input type="hidden" name="enviar" class="form-control" value="si">
+                    <button type="submit" class="btn btn-rounded">Acceder</button>
+                    </button>
                 </form>
             </div>
         </div>
     </div><!--.page-center-->
-
-
 <script src="public/js/lib/jquery/jquery.min.js"></script>
 <script src="public/js/lib/tether/tether.min.js"></script>
 <script src="public/js/lib/bootstrap/bootstrap.min.js"></script>
 <script src="public/js/plugins.js"></script>
-    <script type="text/javascript" src="public/js/lib/match-height/jquery.matchHeight.min.js"></script>
-    <script>
+<script type="text/javascript" src="public/js/lib/match-height/jquery.matchHeight.min.js"></script>
+<script>
         $(function() {
             $('.page-center').matchHeight({
                 target: $('html')
@@ -74,7 +103,7 @@
                 },100);
             });
         });
-    </script>
-<script src="js/app.js"></script>
+</script>
+<script src="public/js/app.js"></script>
 </body>
 </html>
